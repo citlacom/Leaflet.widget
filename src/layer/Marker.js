053bccfade930f4bebf7d8fd94979fb959225a87
Pablo Cerda <pablo@citla.com>
2019-01-01 15:32:51 +0100

    Disable marker tool when cardinality is full.

--- src/layer/Marker.js
+++ src/layer/Marker.js
@@ -13,3 +13,27 @@ L.Marker.include({
         return L.GeoJSONUtil.feature(this.toGeometry());
     }
 });
+
+// Disable marker tool when cardinality is full.
+L.Marker.Draw.include({
+    baseOnMouseMove: L.Marker.Draw.prototype._onMouseMove,
+    _onMouseMove: function (e) {
+        if (!this._map.widget._full) {
+            this.baseOnMouseMove(e);
+        }
+    },
+
+    addHooks: function () {
+        L.Handler.Draw.prototype.addHooks.call(this);
+        if (this._map) {
+          if (!this._map.widget._full) {
+              this._updateLabelText({ text: 'Click map to place marker.' });
+          }
+          else {
+              this._updateLabelText({ text: 'Marker is already set, click and drag it to relocate.' });
+          }
+
+          this._map.on('mousemove', this._onMouseMove, this);
+        }
+    }
+});
