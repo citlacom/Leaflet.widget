00bdb423d8c99eb53d243ad8e444e97308ec7e68
Pablo Cerda <pablo@citla.com>
2019-01-02 21:47:46 +0100

    Dismiss cardinality warning message on map click or marker drag.

--- src/layer/Marker.js
+++ src/layer/Marker.js
@@ -26,14 +26,44 @@ L.Marker.Draw.include({
     addHooks: function () {
         L.Handler.Draw.prototype.addHooks.call(this);
         if (this._map) {
-          if (!this._map.widget._full) {
-              this._updateLabelText({ text: 'Click map to place marker.' });
+          if (this._map.widget._full) {
+              this._updateLabelText({ text: 'Marker is already placed. Click and drag to relocate.' });
+              // Dismiss warning popup when marker or map layers are clicked so
+              // message don't disturb marker relocation.
+              this._map.off('click', this._onClick, this);
+              this._map.on('click', this.dismissWarning, this);
+
+              if (this._map.drawControl.handlers.select.options.selectable) {
+                  var selectable = this._map.drawControl.handlers.select.options.selectable;
+                  selectable.eachLayer(function (layer) {
+                      layer.on('click', this.dismissWarning, this);
+                      layer.on('drag', this.dismissWarning, this);
+                  }, this);
+              }
           }
           else {
-              this._updateLabelText({ text: 'Marker is already set, click and drag it to relocate.' });
+              this._updateLabelText({ text: 'Click map to place marker.' });
           }
 
           this._map.on('mousemove', this._onMouseMove, this);
         }
+    },
+
+    baseRemoveHooks: L.Marker.Draw.prototype.removeHooks,
+    removeHooks: function() {
+        this.baseRemoveHooks();
+        this._map.off('click', this.dismissWarning, this);
+
+        if (this._map.drawControl.handlers.select.options.selectable) {
+            var selectable = this._map.drawControl.handlers.select.options.selectable;
+            selectable.eachLayer(function (layer) {
+                layer.off('click', this.dismissWarning, this);
+                layer.off('drag', this.dismissWarning, this);
+            }, this);
+        }
+    },
+
+    dismissWarning: function(e) {
+        this.disable();
     }
 });
